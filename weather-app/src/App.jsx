0f2a0e0094db70bed4319b23db435db10a333cf4
abubuh <cristian_caht@hotmail.com>
2023-12-28 21:02:17 -0600

    Add routes

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Search from "./pages/Search"
+import CityWeather from "./pages/CityWeather"
+import './index.css'
+
 function App() {
 
   return (
     <>
-      <h1 className="text-2xl">Hello world</h1>
+    <div className="bg-slate-800 w-[100vw] h-[100vh]">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Search/>}/>
+          <Route path="/city/:city" element={<CityWeather/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
     </>
   )
 }
